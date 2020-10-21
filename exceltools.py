fb1bb495b53f9cc9a16db1c8c731273073e8547c
houmingxin <73224859+houmingxin@users.noreply.github.com>
2020-10-21 18:07:39 +0800

    Update exceltools.py

--- exceltools.py
+++ exceltools.py
@@ -28,9 +28,18 @@ def read_excel(excel_path, sheet_name, skip_first=True):
         results.append(table.row_values(row))
 
     return results
+#运行脚本
+#1.pip install allure-pytest -i https://pypi.tuna.tsinghua.edu.cn/simple
+#2.pytest --alluredir=result
+#3.allure generate result -o report --clean
+#4.allure open -h 127.0.0.1 -p 10086 report
+
 
 if __name__ == "__main__":
     excel_path = "C:\\workhome\\test24\\pytesttest24\\data\\测谈网接口测试用例.xlsx"
     sheet_name = "登录"
     a = read_excel(excel_path, sheet_name)
     print(a)
+    
+    
+    
