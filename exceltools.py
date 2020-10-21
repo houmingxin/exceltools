cd3e6ddd9464b3d1a7dfbf79a2dcbe1ea286f86c
houmingxin <73224859+houmingxin@users.noreply.github.com>
2020-10-21 17:39:18 +0800

    Add files via upload

---
+++ exceltools.py
@@ -0,0 +1,36 @@
+"""
+    读取excel
+"""
+import xlrd
+
+def read_excel(excel_path, sheet_name, skip_first=True):
+    """
+        方法：读取excel
+        参数：
+            excel_path: excel的目录
+            sheet_name：表单的名字
+            skip_first：是否跳过首行
+    """
+    results = []
+    # 打开excel
+    datas = xlrd.open_workbook(excel_path)
+    # 获取对应的表单
+    table = datas.sheet_by_name(sheet_name)
+    # 如果skip_first的值为True，start_row= 1> 从excel的第二行开始
+    # 否则，start_row = 0 > 从excel的第一行开始
+    if skip_first == True:
+        start_row = 1
+    else:
+        start_row = 0
+
+    # 循环读取每一行的数据
+    for row in range(start_row, table.nrows):
+        results.append(table.row_values(row))
+
+    return results
+
+if __name__ == "__main__":
+    excel_path = "C:\\workhome\\test24\\pytesttest24\\data\\测谈网接口测试用例.xlsx"
+    sheet_name = "登录"
+    a = read_excel(excel_path, sheet_name)
+    print(a)
